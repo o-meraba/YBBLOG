3f6503e926922e2af7fb5e37d793db72c4a0968e
omeraba <omeraba93@gmail.com>
2022-04-14 15:19:43 +0300

    new codes

--- blog.py
+++ blog.py
@@ -1,29 +1,20 @@
-from distutils.log import debug
+from unicodedata import name
 from flask import Flask,render_template
 
-
-app  = Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", islem = 2)
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return "Hakkımda"
+    return render_template("about.html")
 
-@app.route('/about/omer')
-def omercan():
-    return "Omer"
+@app.route("/articles")
+def articles():
+    return render_template("articles.html")
 
-def method_name():
-    pass
-
-def method_name():
-    pass
-
-def method_name():
-    pass
 if __name__ == "__main__":
     app.run(debug=True)
     
